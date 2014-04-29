040fa739c4b2f33c2ddf529783d498277fa4b01d
roger.yee <roger.yee@hotmail.com>
2014-04-29 14:57:18 +0800

    add Grunt tasks to generate app.min.js for project.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+        options: {
+            separator: ';'//定义一个用于插入合并输出文件之间的字符
+        },
+        dist: {            
+            src: ['webapp/scripts/app/angular/**/*.js'],//用于连接的文件
+            dest: 'webapp/scripts/app/app.min.js'//返回的JS文件位置
+        }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'webapp/scripts/app/app.min.js',
+        dest: 'webapp/scripts/app/app.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
